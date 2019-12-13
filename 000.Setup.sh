0f65d06e725b72d2d371b635173afe5868543f39
Herb Stahl <ghstahl@gmail.com>
2019-12-13 09:59:39 -0800

    update

--- 000.Setup.sh
+++ 000.Setup.sh
@@ -4,12 +4,19 @@ LOCATION="eastus2"
 echo "Positional Parameters"
 echo '$0 = '$0
 echo '$1 = '$1
-subscription_id=$1 
-echo 'subscription_id = '$subscription_id
+SUBSCRIPTION_NAME=$1 
+if [ -z "${SUBSCRIPTION_NAME}" ]; then
+    echo "SUBSCRIPTION_NAME is unset or set to the empty string"
+    SUBSCRIPTION_NAME="MUSE1-NS01"
+fi
+echo 'SUBSCRIPTION_NAME = '$SUBSCRIPTION_NAME
+
+SUBSCRIPTION_ID="$(az account show -s $SUBSCRIPTION_NAME --query id -o tsv)"
+echo 'SUBSCRIPTION_ID = '$SUBSCRIPTION_ID
 
 az aks install-cli
 az login
-az account set --subscription $subscription_id
+az account set --subscription $SUBSCRIPTION_ID
 echo '----- Current Account -----'
 az account show
  
