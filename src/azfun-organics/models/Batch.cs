367696c2077e9cdfbcd459e2f67b590f3511071d
Herb Stahl <ghstahl@gmail.com>
2019-12-17 06:58:04 -0800

    update

---
+++ src/azfun-organics/models/Batch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace azfun_organics.models
+{
+    public class Batch
+    {
+        public string id { get; set; }
+        public string BatchId { get; set; }
+        public List<string> Files { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+}
