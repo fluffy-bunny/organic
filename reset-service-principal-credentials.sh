457f79810168ca662919d06b5274660c4a3f97c8
Herb Stahl <ghstahl@gmail.com>
2019-12-21 15:34:24 -0800

    update new clientid/secret in keyvault

--- reset-service-principal-credentials.sh
+++ reset-service-principal-credentials.sh
@@ -1,3 +1,11 @@
+APP_FRIENDLY_NAME="organics"
+LOCATION="eastus2"
+RESOURCE_GROUP_NAME="rg-$APP_FRIENDLY_NAME-openhack"
+STORAGE_ACCOUNT_NAME="st"$APP_FRIENDLY_NAME"openhack"
+FUNCTION_NAME="azfun-$APP_FRIENDLY_NAME"
+APP_REGISTRATION="appreg-$APP_FRIENDLY_NAME"
+KV_NAME="kv-"$APP_FRIENDLY_NAME
+
 die () {
     echo >&2 "$@"
     echo "$ ./reset-service-principal-credentials.sh [service-principal-name]"
@@ -6,7 +14,27 @@ die () {
 REQUIRED_ARGS=1
 [ "$#" -eq $REQUIRED_ARGS ] || die "$REQUIRED_ARGS argument required, $# provided"
 
-echo '$0 = '$0
-echo '$1 = '$1
+ 
 SP_NAME=$1
-az ad sp credential reset --name $SP_NAME
+CREDENTIALS=$(az ad sp credential reset --name $SP_NAME)
+echo $CREDENTIALS
+
+echo $CREDENTIALS|jq '.appId'
+echo $CREDENTIALS|jq '.name'
+echo $CREDENTIALS|jq '.password'
+echo $CREDENTIALS|jq '.tenant'
+echo "....."
+
+appId=$( echo "$CREDENTIALS" | jq '.["appId"]' )
+name=$( echo "$CREDENTIALS" | jq '.["name"]' )
+password=$( echo "$CREDENTIALS" | jq '.["password"]' )
+tenant=$( echo "$CREDENTIALS" | jq '.["tenant"]' )
+
+echo "appId:$appId"
+echo "name:$name"
+echo "password:$password"
+echo "tenant:$tenant"
+
+sh ./set-keyvault-secret.sh $KV_NAME $FUNCTION_NAME "storage-sp-tenant-id" $tenant
+sh ./set-keyvault-secret.sh $KV_NAME $FUNCTION_NAME "storage-sp-client-id" $appId
+sh ./set-keyvault-secret.sh $KV_NAME $FUNCTION_NAME "storage-sp-client-secret" $password
