f211403653a920cfd58a7fc180dabbcf9aa3ece2
Herb Stahl <ghstahl@gmail.com>
2020-01-07 07:58:58 -0800

    Working send and receive events

--- 002.storage-account.EventHub.sh
+++ 002.storage-account.EventHub.sh
@@ -4,6 +4,9 @@ RESOURCE_GROUP_NAME="rg-$APP_FRIENDLY_NAME-openhack"
 STORAGE_ACCOUNT_NAME="st"$APP_FRIENDLY_NAME"eventhub"
 SKU="Standard_LRS"
 KIND="StorageV2"
+EVENTHUB_NAMESPACE="evhns-$APP_FRIENDLY_NAME"
+EVENTHUB_NAME="evh-$APP_FRIENDLY_NAME"
+
 echo "=== Create Storage Account: $STORAGE_ACCOUNT_NAME in ResourceGroup $RESOURCE_GROUP_NAME at Location $LOCATION ==="
 az storage account create \
     -n $STORAGE_ACCOUNT_NAME \
@@ -28,8 +31,7 @@ az storage container create \
     --account-name $STORAGE_ACCOUNT_NAME \
     --account-key $AZURE_STORAGE_ACCESS_KEY  
 
-EVENTHUB_NAMESPACE="ns-$APP_FRIENDLY_NAME-eventhub"
-EVENTHUB_NAME="myeventhubname"
+
 echo EVENTHUB_NAMESPACE: $EVENTHUB_NAMESPACE
 echo EVENTHUB_NAME: $EVENTHUB_NAME
 
@@ -47,3 +49,7 @@ az eventhubs eventhub create \
      --message-retention 1 \
      --partition-count 2
  
+# az eventhubs namespace authorization-rule keys list --resource-group rg-organics-openhack --namespace-name evhns-organics --name RootManageSharedAccessKey
+
+CONNECTION_STRING=$(az eventhubs namespace authorization-rule keys list --namespace-name $EVENTHUB_NAMESPACE --name RootManageSharedAccessKey --resource-group $RESOURCE_GROUP_NAME --query primaryConnectionString --output tsv)
+echo CONNECTION_STRING: $CONNECTION_STRING 
--- src/SendEventTestClient/Program.cs
+++ src/SendEventTestClient/Program.cs
@@ -12,6 +12,10 @@ namespace SendEventTestClient
     class Program
     {
         private static EventHubClient eventHubClient;
+        private static bool SetRandomPartitionKey = false;
+        private static string EventHubConnectionString = "Event Hubs namespace connection string";
+        private static string EventHubName = "event hub name";
+
         static async Task Main(string[] args)
         {
             var helloWorld = await GetHelloWorldAsync();
@@ -36,29 +40,51 @@ namespace SendEventTestClient
             var mySettings = config.GetSection("mySettings").Get<MySettings>();
             Console.WriteLine($"EventHubName:{mySettings.EventHubName}");
             Console.WriteLine($"EventHubConnectionString:{mySettings.EventHubConnectionString}");
-            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
-            // Typically, the connection string should have the entity path in it, but this simple scenario
-            // uses the connection string from the namespace.
-            var connectionStringBuilder = new EventHubsConnectionStringBuilder(mySettings.EventHubConnectionString)
+            EventHubName = mySettings.EventHubName;
+            EventHubConnectionString = mySettings.EventHubConnectionString;
+
+            // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
+            // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
             {
-                EntityPath = mySettings.EventHubName
+                EntityPath = EventHubName
             };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
 
             await SendMessagesToEventHub(100);
 
             await eventHubClient.CloseAsync();
 
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
             return $" Hello { config["name"] } and {mySettings.Name} !";
         }
+        // Creates an Event Hub client and sends 100 messages to the event hub.
         private static async Task SendMessagesToEventHub(int numMessagesToSend)
         {
+            var rnd = new Random();
+
             for (var i = 0; i < numMessagesToSend; i++)
             {
                 try
                 {
                     var message = $"Message {i}";
-                    Console.WriteLine($"Sending message: {message}");
-                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+
+                    // Set random partition key?
+                    if (SetRandomPartitionKey)
+                    {
+                        var pKey = Guid.NewGuid().ToString();
+                        await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)), pKey);
+                        Console.WriteLine($"Sent message: '{message}' Partition Key: '{pKey}'");
+                    }
+                    else
+                    {
+                        await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                        Console.WriteLine($"Sent message: '{message}'");
+                    }
                 }
                 catch (Exception exception)
                 {
